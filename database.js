d77bb1148903d0f704e6f532f35e9b39bb299bd6
k783s4 <chipleo.codes@gmail.com>
2020-04-24 23:33:57 +0200

    added database.js

---
+++ database.js
@@ -0,0 +1,30 @@
+let mysql = require("mysql")
+
+module.exports.query = (query, con, callback) => {
+  //requires and execute query
+  con.query(query, function (err, result, fields) {
+    if (err) throw err;
+    //return query results
+    return callback(result)
+  });
+}
+
+module.exports.connect = () => {
+  connection = mysql.createConnection({
+          host     : 'localhost',
+          user     : 'backend',
+          password : '!attention3L',
+          database : 'nocostream'
+      });
+      //if connection fails
+      connection.on('error', function() {console.log("ERROR")});
+      //connect to database with pre-made settigns
+      connection.connect();
+      //return the connection variable to the controller
+      return connection
+}
+
+module.exports.checkforcleaninput = (str) =>{
+  let unallowed = []
+  return True;
+}
--- login.js
+++ login.js
@@ -1,21 +1,45 @@
 let express = require("express")
 let crypto = require("crypto")
+let readline = require("readline");
+let database = require("./database.js")
+
 let app = express()
 
 //define app as using JSON
 app.use(express.json());
 
+//connect to database
+let con = database.connect();
+
+//set up for exit
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.on("close", function() {
+    console.log("stopping service");
+    con.end()
+    process.exit(0);
+});
+
 //on POST request
 app.post('/', function(req, res){
+  //grab login data
       let username = req.body.username;
       let password_unhashed = req.body.password;
+
+      //clean input
+      if(database.checkforcleaninput(username) && database.checkforcleaninput(password_unhashed)){} else{res.end("Bad Input")}
 
       //create password hasher
       let sha256hasher = crypto.createHash("sha256");
       //hash the password
       let password_hashed = sha256hasher.update(password_unhashed).digest("hex");
 
-      console.log(password_hashed)
+  //check if login data is correct
+      let result = database.query("SELECT * FROM streamers", con, (result) =>{
+        console.log(result)
+      })
   });
 
 app.listen(8000, () => console.log("running"));
