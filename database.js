ffcc02a4ef03c2a0a1641518a6cb2102f7ebd9f6
k783s4 <chipleo.codes@gmail.com>
2020-04-25 18:28:15 +0200

    Added basic SQLi prevention and beautified a bit

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ let mysql = require("mysql")
 
 module.exports.query = (query, con, callback) => {
   //requires and execute query
-  con.query(query, function (err, result, fields) {
+  con.query(query, function(err, result, fields) {
     if (err) throw err;
     //return query results
     return callback(result)
@@ -11,20 +11,31 @@ module.exports.query = (query, con, callback) => {
 
 module.exports.connect = () => {
   connection = mysql.createConnection({
-          host     : 'localhost',
-          user     : 'backend',
-          password : '!attention3L',
-          database : 'nocostream'
-      });
-      //if connection fails
-      connection.on('error', function() {console.log("ERROR")});
-      //connect to database with pre-made settigns
-      connection.connect();
-      //return the connection variable to the controller
-      return connection
+    host: 'localhost',
+    user: 'backend',
+    password: '!attention3L',
+    database: 'nocostream'
+  });
+  //if connection fails
+  connection.on('error', function() {
+    console.log("ERROR")
+  });
+  //connect to database with pre-made settigns
+  connection.connect();
+  //return the connection variable to the controller
+  return connection
 }
 
-module.exports.checkforcleaninput = (str) =>{
-  let unallowed = []
-  return true;
+module.exports.checkforcleaninput = (str) => {
+  const regex = /^[a-zA-Z]+$/g;
+  let found = str.match(regex);
+
+  if (found !== null && found.length === 1) {
+    if (found[0] === str) {
+      return true;
+    }
+  }
+
+  return false;
+
 }
--- login.js
+++ login.js
@@ -13,13 +13,13 @@ let con = database.connect();
 
 //set up for exit
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 rl.on("close", function() {
-    console.log("stopping service");
-    con.end()
-    process.exit(0);
+  console.log("stopping service");
+  con.end()
+  process.exit(0);
 });
 
 //precalculate date for cookie session expiry
@@ -32,72 +32,77 @@ let dd = someDate.getDate()
 let expiry_date = yyyy + "/" + mm + "/" + dd;
 
 //on POST request
-app.post('/', function(req, res){
+app.post('/', function(req, res) {
   //grab login data
-      let username = req.body.username;
-      let password_unhashed = req.body.password;
+  let username = req.body.username;
+  let password_unhashed = req.body.password;
 
-      //clean input
-      if(database.checkforcleaninput(username) && database.checkforcleaninput(password_unhashed)){} else{res.end("Bad Input")}
+  //clean input
+  if (database.checkforcleaninput(username) && database.checkforcleaninput(password_unhashed)) {} else {
+    res.end("Bad Input")
+  }
 
-      //create password hasher
-      let sha256hasher = crypto.createHash("sha256");
-      //hash the password
-      let password_hashed = sha256hasher.update(password_unhashed).digest("hex");
+  //create password hasher
+  let sha256hasher = crypto.createHash("sha256");
+  //hash the password
+  let password_hashed = sha256hasher.update(password_unhashed).digest("hex");
 
   //check if login data is correct
-      database.query(`SELECT * FROM streamers WHERE username='${username}' AND password_hash='${password_hashed}';`, con, (result) =>{
+  database.query(`SELECT * FROM streamers WHERE username='${username}' AND password_hash='${password_hashed}';`, con, (result) => {
 
-        if(result.length === 1){
-          //if it is correct grab user_id
-          let streamer_id = result[0].streamer_id
+    if (result.length === 1) {
+      //if it is correct grab user_id
+      let streamer_id = result[0].streamer_id
 
-          //create session_cookie and pass it to the user
-          generateCookie((n_cookie) =>{
-            console.log("NCOOK: " + n_cookie);
-            database.query(`INSERT INTO stream_sessions (streamer_session, session_invalid_after, streamer_id) VALUES ('${n_cookie}', '${expiry_date}', '${streamer_id}')`, con, (result2) =>{
-              console.log(result2);
-              res.writeHead(200,{"content-type" : "text/html"});
-              res.end(n_cookie);
-            });
+      //create session_cookie and pass it to the user
+      generateCookie((n_cookie) => {
+        console.log("NCOOK: " + n_cookie);
+        database.query(`INSERT INTO stream_sessions (streamer_session, session_invalid_after, streamer_id) VALUES ('${n_cookie}', '${expiry_date}', '${streamer_id}')`, con, (result2) => {
+          console.log(result2);
+          res.writeHead(200, {
+            "content-type": "text/html"
           });
-
-        }
-        else if(result.length > 1){
-          //if something went horribly wrong - TODO: Properly report
-          console.log("WARNING: CRITICAL ERROR " + result)
-          res.writeHead(401,{"content-type" : "text/html"});
-          res.end("Invalid credentials");
-        }
-        else{
-          //in case username and password are wrong
-          res.writeHead(401,{"content-type" : "text/html"});
-          res.end("Invalid credentials");
-        }
+          res.end(n_cookie);
+        });
       });
+
+    } else if (result.length > 1) {
+      //if something went horribly wrong - TODO: Properly report
+      console.log("WARNING: CRITICAL ERROR " + result)
+      res.writeHead(401, {
+        "content-type": "text/html"
+      });
+      res.end("Invalid credentials");
+    } else {
+      //in case username and password are wrong
+      res.writeHead(401, {
+        "content-type": "text/html"
+      });
+      res.end("Invalid credentials");
+    }
   });
+});
 
 let i = 0;
-function generateCookie(callback){
+
+function generateCookie(callback) {
   let new_sess_cookie = crypto.randomBytes(16).toString('hex');
   console.log("SC: " + new_sess_cookie);
   //check if cookie already exists
-  database.query(`SELECT * FROM stream_sessions WHERE streamer_session='${new_sess_cookie}';`, con, (result3) =>{
+  database.query(`SELECT * FROM stream_sessions WHERE streamer_session='${new_sess_cookie}';`, con, (result3) => {
     //if no cookie is already found return it
-    if(result3.length === 0){
-        i = 0;
-        console.log("return");
-        return callback(new_sess_cookie);
-    }
-    else{
-      if(i > 10){
+    if (result3.length === 0) {
+      i = 0;
+      console.log("return");
+      return callback(new_sess_cookie);
+    } else {
+      if (i > 10) {
         console.log("fatal error in cookie generation");
+      } else {
+        generateCookie(callback);
+        //TODO : to be tested
+        i++;
       }
-      else{
-      generateCookie(callback);
-      //TODO : to be tested
-      i++;
-    }
     }
   });
 }
